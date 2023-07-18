ad4a1fe7d34c46cd337c4725e2b8a126cce1ee52
dan lima <danieldelimamendes500@gmail.com>
2023-07-18 12:38:14 -0300

    Initial commit

---
+++ src/main/java/com/dn/chat/websocket/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.dn.chat.websocket;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ src/main/java/com/dn/chat/websocket/MessageType.java
@@ -0,0 +1,8 @@
+package com.dn.chat.websocket;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
